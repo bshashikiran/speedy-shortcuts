a0500eed829aa27352f17155665bb908ca2c3771
Shashi Kiran <shashi.bolleddula@magicbricks.com>
2025-02-26 20:10:29 +0530

    Update: Quick open with selected text

--- src/extension.js
+++ src/extension.js
@@ -3,11 +3,31 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate() { }
+function activate(context) {
+	console.log('Speedy Shortcuts is now active!');
+
+	let disposableShift = vscode.commands.registerCommand("speedyShortcuts.doubleTap.shift", () => {
+		vscode.commands.executeCommand("workbench.action.findInFiles");
+	});
+
+	let disposableCtrl = vscode.commands.registerCommand("speedyShortcuts.doubleTap.ctrl", () => {
+		vscode.commands.executeCommand("workbench.action.showCommands");
+	});
+
+	let disposableAlt = vscode.commands.registerCommand("speedyShortcuts.doubleTap.alt", () => {
+		const editor = vscode.window.activeTextEditor;
+		const selectedText = editor ? editor.document.getText(editor.selection).trim() : '';
+
+		if (selectedText) {
+			vscode.commands.executeCommand("workbench.action.quickOpen", selectedText);
+		} else {
+			vscode.commands.executeCommand("workbench.action.quickOpen");
+		}
+	});
+
+	context.subscriptions.push(disposableShift, disposableCtrl, disposableAlt);
+}
 
 function deactivate() { }
 
-module.exports = {
-	activate,
-	deactivate
-}
+module.exports = { activate, deactivate };
